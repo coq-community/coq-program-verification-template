16d4ebeb5041559e1a28d453c01c7fa6a86bd0d4
Karl Palmskog <palmskog@gmail.com>
2020-09-08 05:42:10 -0500

    simplify contract

--- theories/binary_search_verif.v
+++ theories/binary_search_verif.v
@@ -11,31 +11,30 @@ Definition binary_search_spec :=
   WITH a: val, sh : share, contents : list Z, len : Z, key : Z
   PRE [ tptr tint, tint, tint ]
     PROP (readable_share sh;
-          Zlength contents <= Int.max_signed;
-          len = Zlength contents;
+          0 <= len <= Int.max_signed;
           Int.min_signed <= key <= Int.max_signed;
           Forall (fun x => Int.min_signed <= x <= Int.max_signed) contents;
           sorted contents)
     PARAMS (a; Vint (Int.repr len); Vint (Int.repr key))
-    SEP (data_at sh (tarray tint (Zlength contents)) (map Vint (map Int.repr contents)) a)
+    SEP (data_at sh (tarray tint len) (map Vint (map Int.repr contents)) a)
   POST [ tint ]
     EX i:Z,
      PROP (if in_dec Z.eq_dec key contents then Znth i contents = key
            else insertion_point (-i - 1) contents key)
      RETURN (Vint (Int.repr i))
-     SEP (data_at sh (tarray tint (Zlength contents)) (map Vint (map Int.repr contents)) a).
+     SEP (data_at sh (tarray tint len) (map Vint (map Int.repr contents)) a).
 
 Definition binary_search_while_spec a sh contents len key :=
  EX low : Z, EX high : Z,
   PROP (0 <= low <= len;
-   Int.min_signed <= high < len;
-   In key contents -> In key (sublist low (high + 1) contents);
-   ~ In key contents ->
-    Forall (fun x => x < key) (sublist 0 low contents) /\
-    Forall (fun x => key < x) (sublist (high + 1) len contents))
+        Int.min_signed <= high < len;
+        In key contents -> In key (sublist low (high + 1) contents);
+        ~ In key contents ->
+         Forall (fun x => x < key) (sublist 0 low contents) /\
+         Forall (fun x => key < x) (sublist (high + 1) len contents))
   LOCAL (temp _a a; temp _key (Vint (Int.repr key));
          temp _low (Vint (Int.repr low)); temp _high (Vint (Int.repr high)))
-  SEP (data_at sh (tarray tint (Zlength contents)) (map Vint (map Int.repr contents)) a).
+  SEP (data_at sh (tarray tint len) (map Vint (map Int.repr contents)) a).
 
 Definition main_spec :=
  DECLARE _main
@@ -57,6 +56,7 @@ forward_while (binary_search_while_spec a sh contents len key).
   assert (Heql: Zlength contents - 1 + 1 = Zlength contents) by lia.
   rewrite Heql; clear Heql.
   rewrite sublist_same; auto.
+  list_simplify; [rep_lia|].
   rewrite sublist_nil; rewrite sublist_nil; auto.
 - entailer!.
 - assert (Hle: low <= high) by lia.
@@ -73,6 +73,14 @@ forward_while (binary_search_while_spec a sh contents len key).
     change (two_p (Int.unsigned (Int.repr 1))) with 2.
     rewrite !Int.signed_repr; try rep_lia; [rewrite !Int.unsigned_repr; rep_lia|].
     rewrite !Int.unsigned_repr; rep_lia.
+  assert_PROP (0 <= Int.unsigned (Int.shru (Int.repr (low + high)) (Int.repr 1)) /\
+   Int.unsigned (Int.shru (Int.repr (low + high)) (Int.repr 1)) < Zlength (map Int.repr contents))
+   as Hlen.
+    entailer!.
+    rewrite Heq.
+    list_solve.
+  assert (Hlen': 0 <= Int.unsigned (Int.shru (Int.repr (low + high)) (Int.repr 1)) < Zlength contents)
+   by list_solve.
   forward.
   forward.
   forward_if.
@@ -80,27 +88,27 @@ forward_while (binary_search_while_spec a sh contents len key).
     entailer!.
     Exists ((low + high)/2 + 1,high).
     entailer!.
-    rewrite Heq in H8.
-    split; [lia|]; rewrite !Int.signed_repr in H8; [split;[|split]|].
-    + intro H0; specialize (H6 H0).
-      eapply In_sorted_gt; eauto; lia.
-    + intro H0; specialize (H7 H0).
-      destruct H7 as [Hlow Hhigh].
+    rewrite Heq in H7.
+    split; [lia|]; rewrite !Int.signed_repr in H7; [split;[|split]|].
+    + intro Hkey; specialize (H5 Hkey).
+      eapply In_sorted_gt; eauto; list_solve.
+    + intro Hkey; specialize (H6 Hkey).
+      destruct H6 as [Hlow Hhigh].
       split; [|assumption].
       apply Forall_forall; intros.
-      eapply Znth_lt_sublist_lt; eauto; lia.
+      eapply Znth_lt_sublist_lt; eauto; list_solve.
     + f_equal.
       unfold Int.add at 1.
       f_equal.
       rewrite <- Heq.
       rewrite !Int.unsigned_repr; rep_lia.
     + assert (Hin: In (Znth ((low + high) / 2) contents) contents)
-       by (eapply Znth_In; eauto; lia).
+       by (eapply Znth_In; eauto; list_solve).
       pose proof (Forall_forall (fun x : Z =>
        Int.min_signed <= x <= Int.max_signed) contents) as HFA.
       simpl in HFA.
       destruct HFA as [HFA1 HFA2].
-      apply (HFA1 H2 _ Hin).
+      apply (HFA1 H1 _ Hin).
   * forward_if.
     + forward.
       entailer!.
@@ -108,17 +116,19 @@ forward_while (binary_search_while_spec a sh contents len key).
       entailer!.
       assert (Heqlh: (low + high) / 2 - 1 + 1 = (low + high) / 2) by lia.
       rewrite Heqlh; clear Heqlh.
-      rewrite Heq in H9.
-      split; [rep_lia|]; rewrite !Int.signed_repr in H9; [split;[|split]|].
-      -- intro H0.
-         specialize (H6 H0).
-         eapply (In_sorted_lt _ _ _ _ _ (high + 1)); eauto; lia.
-      -- intro H0.
-         specialize (H7 H0).
-         destruct H7 as [Hl Hh].
+      rewrite Heq in H8.
+      split; [rep_lia|]; rewrite !Int.signed_repr in H8; [split;[|split]|].
+      -- intro Hkey.
+         specialize (H5 Hkey).
+         eapply (In_sorted_lt _ _ _ _ _ (high + 1)); eauto; list_solve.
+      -- intro Hkey.
+         specialize (H6 Hkey).
+         destruct H6 as [Hl Hh].
          split; [assumption|].
          apply Forall_forall; intros.
-         revert H7.
+         revert H6.
+         list_simplify.
+         revert H6.
          apply Znth_gt_sublist_gt; auto; lia.
       -- f_equal.
          unfold Int.sub.
@@ -126,19 +136,19 @@ forward_while (binary_search_while_spec a sh contents len key).
          rewrite !Int.unsigned_repr; [|rep_lia].
          rewrite Heq; reflexivity.
       -- assert (Hin: In (Znth ((low + high) / 2) contents) contents)
-          by (eapply Znth_In; eauto; lia).
+          by (eapply Znth_In; eauto; list_solve).
          pose proof (Forall_forall (fun x : Z =>
           Int.min_signed <= x <= Int.max_signed) contents) as HFA.
          simpl in HFA.
          destruct HFA as [HFA1 HFA2].
-         apply (HFA1 H2 _ Hin).
+         apply (HFA1 H1 _ Hin).
     + forward.
       Exists ((low + high) / 2).
       entailer!.
-      revert H8 H9.
+      revert H7 H8.
       rewrite Heq.
       rewrite !Int.signed_repr.
-      -- intros H8 H9.
+      -- intros H7 H8.
          assert (Hkey: Znth ((low + high) / 2) contents = key) by lia.
          case (in_dec _ _); intro; [split|]; auto.
          ** f_equal.
@@ -149,15 +159,15 @@ forward_while (binary_search_while_spec a sh contents len key).
             reflexivity.
          ** contradict n.
             assert (Hin: In (Znth ((low + high) / 2) contents) contents)
-              by (eapply Znth_In; eauto; lia).
+              by (eapply Znth_In; eauto; list_solve).
             rewrite Hkey in Hin; assumption.
       -- assert (Hin: In (Znth ((low + high) / 2) contents) contents)
-          by (eapply Znth_In; eauto; lia).
+          by (eapply Znth_In; eauto; list_solve).
          pose proof (Forall_forall (fun x : Z =>
           Int.min_signed <= x <= Int.max_signed) contents) as HFA.
          simpl in HFA.
          destruct HFA as [HFA1 HFA2].
-         apply (HFA1 H2 _ Hin).
+         apply (HFA1 H1 _ Hin).
 - forward.
   + entailer!.
     set (j := Int.signed Int.zero) in *; compute in j; subst j.
@@ -165,19 +175,19 @@ forward_while (binary_search_while_spec a sh contents len key).
   + Exists (-low - 1).
     entailer!.
     case (in_dec _ _); intro.
-    * specialize (H6 i).
-      contradict H6.
+    * specialize (H5 i).
+      contradict H5.
       assert (Hlow: high + 1 <= low) by lia.
       rewrite sublist_nil1; auto.
     * unfold insertion_point.
       assert (Hl: - (- low - 1) - 1 = low) by lia.
       rewrite Hl.
-      specialize (H7 n).
-      destruct H7.
-      split; [lia|].
+      specialize (H6 n).
+      destruct H6.
+      split; [list_solve|].
       split; auto.
       assert (Hle: high + 1 <= low) by lia.
-      eapply Forall_sublist_overlap; eauto.
+      eapply Forall_sublist_overlap; eauto; list_solve.
 Qed.
 
 Definition four_contents := [1; 2; 3; 4].
